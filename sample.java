452acd09fbd823561896127c752d5d70bd9d0e77
Ramachandran <134304263+Ram24102002@users.noreply.github.com>
2024-08-02 09:42:34 +0530

    amend

---
+++ sample.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int j=0;
+        int i;
+        for(i=0;i<=x.length;i++);
+        {
+           j=j+i;
+        }
+        System.out.println(j);
+    }
+}
